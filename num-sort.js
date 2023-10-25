0daafefc6e2ad423c4841320f3232bac07c91e36
Elizabeth Rice <elizabethclararice@outlook.com>
2023-10-24 19:38:41 -0500

    first commit

---
+++ num-sort.js
@@ -0,0 +1,68 @@
+let numSort = function(arr) {
+    let evens = [];
+    let div5s = [];
+    let primes = [];
+
+
+    for (let num of arr) {
+        let even = isEven(num);
+        let div5 = isDiv5(num);
+        let prime = isPrime(num);
+
+        if (even && num !== 0) {
+            evens.push(num);
+        }
+
+        if (div5 && num !== 0) {
+            div5s.push(num);
+        }
+
+        if (prime) {
+            primes.push(num);
+        }
+    }
+
+    let evensStr = evens.join(', ');
+    let div5sStr = div5s.join(', ');
+    let primesStr = primes.join(', ');
+
+    let tempLiteral = `Even numbers: ${evensStr}
+    Numbers divisible by 5: ${div5sStr}
+    Prime numbers: ${primesStr}`;
+
+    console.log('Even numbers: ' + evensStr);
+    console.log('Numbers divisible by 5: ' + div5sStr);
+    console.log('Prime numbers: ' + primesStr);
+
+    // tempLiteral can used as a return statement if desired as an alternative to the console.logs
+};
+
+
+let isEven = function(num) {
+    return num % 2 === 0;
+};
+
+let isDiv5 = function(num) {
+    return num % 5 === 0;
+};
+
+let isPrime = function(num) {
+    if (num <= 1) {
+        return false;
+    }
+
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+
+    return true;
+};
+
+
+let arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+numSort(arr1);
+
+let arr2 = [9, 14, 22, 42, 6, 2, 97, 28, 5, 222, 0];
+numSort(arr2);
